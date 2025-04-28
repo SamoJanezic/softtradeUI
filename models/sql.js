e93367c9c2689a4226c9e9bdb4133956463d7c2c
SamoJanezic <janezic.orion@gmail.com>
2025-04-28 14:59:12 +0200

    added update to sql

--- models/sql.js
+++ models/sql.js
@@ -17,3 +17,20 @@ export function updateKategorija(marza, id) {
 	const stmt = db.prepare(sql)
 	return stmt.run(marza, id)
 }
+
+export function updateIzdelek(obj) {
+	const sql = `UPDATE IZDELEK_DOBAVITELJ SET KATEGORIJA_kategorija = ?, izdelek_ime = ?, izdelek_opis = ?, izdelek_kratki_opis = ?, dealer_cena = ?, nabavna_cena = ?, ppc = ?, zaloga = ?, aktiven = ? WHERE id = ?;`
+	const stmt = db.prepare(sql)
+	return stmt.run(
+		obj.KATEGORIJA_kategorija,
+		obj.izdelek_ime,
+		obj.izdelek_opis,
+		obj.izdelek_kratki_opis,
+		obj.dealer_cena,
+		obj.nabavna_cena,
+		obj.ppc,
+		obj.zaloga,
+		obj.aktiven,
+		obj.id
+	)
+}
